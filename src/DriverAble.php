a53b9c8bc735ba4a48f9134ea61cb3006e37d36a
Radu <radu.bordei89@gmail.com>
2015-09-08 17:01:55 +0300

    adding two required functions.

--- src/DriverAble.php
+++ src/DriverAble.php
@@ -18,4 +18,24 @@ interface DriverAble {
      * @return mixed
      */
     public function getSdk();
+
+    /**
+     * Get total visitors for specific period .
+     *
+     * @param string $start
+     * @param string $end
+     * @param string $max
+     * @return mixed
+     */
+    public function totalVisitors($start, $end = null, $max = null);
+
+    /**
+     * Get total views for specific period .
+     *
+     * @param string $start
+     * @param string $end
+     * @param string $max
+     * @return mixed
+     */
+    public function totalViews($start, $end = null, $max = null);
 }
