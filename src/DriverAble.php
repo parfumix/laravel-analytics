f0930a264ddb7547e61f4897f844ba67a3fd55e7
Radu <radu.bordei89@gmail.com>
2015-09-08 14:36:42 +0300

    improve contract.

--- src/DriverAble.php
+++ src/DriverAble.php
@@ -4,4 +4,18 @@ namespace Laravel\Analytics;
 
 interface DriverAble {
 
+    /**
+     * Set sdk .
+     *
+     * @param $sdk
+     * @return mixed
+     */
+    public function setSdk($sdk);
+
+    /**
+     * Get skd .
+     *
+     * @return mixed
+     */
+    public function getSdk();
 }
