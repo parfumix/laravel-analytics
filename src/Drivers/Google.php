8b6b9ad546f01ad83ebbfda476e896b4507a7852
Radu <radu.bordei89@gmail.com>
2015-09-08 14:36:57 +0300

    improve buildSdk google.

--- src/Drivers/Google.php
+++ src/Drivers/Google.php
@@ -2,8 +2,38 @@
 
 namespace Laravel\Analytics\Drivers;
 
+use Google_Auth_AssertionCredentials;
+use Google_Client;
+use Google_Service_Analytics;
+use Laravel\Analytics\Driver;
 use Laravel\Analytics\DriverAble;
 
-class Google implements DriverAble {
+class Google extends Driver implements DriverAble {
 
+    /**
+     * Set sdk .
+     *
+     * @return mixed
+     */
+    public function buildSdk() {
+        $client = new Google_Client();
+        $client->setApplicationName('My cool application');
+        $client->setAssertionCredentials(
+            new Google_Auth_AssertionCredentials(
+                $this->get('email_address'),
+                array('https://www.googleapis.com/auth/analytics.readonly'),
+                file_get_contents(storage_path(
+                    $this->get('certificate')
+                ))
+            ));
+
+        $client->setClientId($this->get('clien_id'));
+        $client->setAccessType('offline_access');
+
+        $service = new Google_Service_Analytics($client);
+
+        $this->setSdk($service);
+
+        return $this;
+    }
 }
