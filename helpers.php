cd13696a6f8a00da25690df915a25231f1766dc1
Radu <radu.bordei89@gmail.com>
2015-09-08 17:02:18 +0300

    adding helper functions.

--- helpers.php
+++ helpers.php
@@ -12,3 +12,31 @@ function analytic($driver = '') {
     return app('analytic-manager')
         ->driver($driver);
 }
+
+/**
+ * Get total visitors .
+ *
+ * @param string $driver
+ * @param string $start
+ * @param string $end
+ * @param string $max
+ * @return mixed
+ */
+function total_visitors($driver = '', $start = '', $end = '', $max = '') {
+    return analytic($driver)
+        ->totalVisitors($start, $end, $max);
+}
+
+/**
+ * Get total views for specific period .
+ *
+ * @param string $driver
+ * @param string $start
+ * @param string $end
+ * @param string $max
+ * @return mixed
+ */
+function total_views($driver = '', $start = '', $end = '', $max = '') {
+    return analytic($driver)
+        ->totalViews($start, $end, $max);
+}
